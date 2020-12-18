70e7f21777ed1aa53cd495857408614f60d9f94a
praza24 <prasathsrithar@hotmail.co.uk>
2020-12-18 19:23:22 +0000

    Initial Commit

---
+++ RxSwiftMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwiftMVVM
+//
+//  Created by Prasath Srithar on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
